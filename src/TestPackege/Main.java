235295d942e6b1ab7b73fcfd8f9359cb0fd6d84f
Andrei Ciuvaga <andrei.ciuvaga@gmail.com>
2019-11-21 23:38:30 +0200

    Initial commit Main method added

--- src/TestPackege/Main.java
+++ src/TestPackege/Main.java
@@ -1,7 +1,36 @@
 package TestPackege;
 
+import java.util.ArrayList;
+
+class GenericsDemo<T> {
+    T values;
+
+    public void show() {
+        System.out.println(values.getClass().getName());
+
+    }
+
+    public void setValue(T values) {
+        this.values = values;
+    }
+
+    public T getValues(){
+        return values;
+    }
+    public void demo(ArrayList <? super T> obj) {
+        System.out.println(1);
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        GenericsDemo<Integer> obj = new GenericsDemo<>();
+        //obj.show();
+        obj.demo(new ArrayList<Number>());
+
+
     }
 }
